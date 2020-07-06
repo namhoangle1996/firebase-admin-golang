9f4ac0b10d025751ff96d6785112e295a2415329
namhoangle <happybear02042000@gmail.com>
2020-07-06 10:56:18 +0700

    firebase-admin read list users

--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"context"
 	firebase "firebase.google.com/go"
+	"firebase.google.com/go/auth"
+	"fmt"
+	"google.golang.org/api/iterator"
+
 	//"fmt"
 	"google.golang.org/api/option"
 )
 
-func main()  {
+func main() {
 	type Account struct {
-		Name string `json:"name"`
-		Balance int64 `json:"balance"`
+		Name    string `json:"name"`
+		Balance int64  `json:"balance"`
 	}
 
 	config := &firebase.Config{
@@ -23,11 +27,42 @@ func main()  {
 		panic(err)
 	}
 
-	client, err := app.Database(context.Background())
+	//client, err := app.Database(context.Background())
+	authCl, err := app.Auth(context.Background())
 	if err != nil {
 		panic(err)
 	}
-	_ = client
+	//_ = client
+	ctx := context.Background()
+	iter := authCl.Users(ctx, "")
+	for {
+		user, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			fmt.Printf("error listing users: %s\n", err)
+		}
+		fmt.Printf("read user user: %s\n", user)
+	}
+
+	// Iterating by pages 100 users at a time.
+	// Note that using both the Next() function on an iterator and the NextPage()
+	// on a Pager wrapping that same iterator will result in an error.
+	pager := iterator.NewPager(authCl.Users(ctx, ""), 100, "")
+	for {
+		var users []*auth.ExportedUserRecord
+		nextPageToken, err := pager.NextPage(&users)
+		if err != nil {
+			fmt.Print(err)
+		}
+		for _, u := range users {
+			fmt.Printf("read user user: %v\n", u)
+		}
+		if nextPageToken == "" {
+			break
+		}
+	}
 
 	// @todo : set
 	//acc := Account{
@@ -47,16 +82,15 @@ func main()  {
 	//fmt.Printf("get account name %s \n",getAcc.Name)
 	//fmt.Printf("get account balance %.2f",getAcc.Balance)
 
-
 	// @todo : push
 	//arr1 := 200
 	//val, err := client.NewRef("push/arr").Push(context.Background(), arr1)
 	//if err != nil {
 	//	panic(err)
 	//}
- 	//_ = val
+	//_ = val
 
- 	// @todo verfify token email
+	// @todo verfify token email
 	//idToken := "AM5PThCtCl0XQHw_oLvS6_7hio0mm7rbS1oba4dhzd5qUwqJccjMURGatozp0ZS-TbW09GJ3sokAs30H3a8jRAaLxAib31v0KSf8mEa6EzMjcKHymyqprg9zXdy4yDUI4J5CXmL3USsgSxdf7-ZTldn4NLTPDByQ_A"
 	//getVerify, err := client.VerifyIDToken(context.Background(), idToken)
 	//if err != nil {
@@ -65,7 +99,4 @@ func main()  {
 	//	log.Log(getVerify)
 	//}
 
-
-
 }
-
